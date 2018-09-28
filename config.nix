52865e4131cb1b2da078ef1655f0528b19e7ee27
Chris Atkin <atkinchris@gmail.com>
2018-09-28 16:03:51 +0100

    Update config for default Gnome

--- config.nix
+++ config.nix
@@ -5,32 +5,16 @@ with lib;
 
 {
   imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
 
   time.timeZone = "Europe/London";
 
   services.xserver = {
-    enable = true;
-
     layout = "gb";
     xkbVariant = "mac";
-
-    desktopManager.gnome3.enable = true;
-    displayManager.gdm.autoLogin = {
-      enable = true;
-      user = "root";
-    };
   };
 
   users.mutableUsers = false;
-
-  networking.networkmanager.enable = true;
-  networking.wireless.enable = mkForce false;
-  powerManagement.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    vim
-  ];
 }
